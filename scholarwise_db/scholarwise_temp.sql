450e07b70ad260579a502a12b94a32971caa3bd4
Abhishek <abhisoni1764@gmail.com>
2024-04-30 22:01:08 +0530

    database updated

--- scholarwise_db/scholarwise_temp.sql
+++ scholarwise_db/scholarwise_temp.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `scholarwise_temp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `scholarwise_temp`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: scholarwise_temp
@@ -722,4 +724,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-30 21:54:28
+-- Dump completed on 2024-04-30 22:00:26
